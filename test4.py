d92d12a970a26d635d4227fbdf27a80f0a520249
Fidel Panal Jr <60313258+nckpnl@users.noreply.github.com>
2023-04-24 13:27:34 +0800

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,72 @@
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+# Set up GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+# Function to measure distance using ultrasonic sensor
+def measure_distance():
+    # Send 10us pulse to trigger pin
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    # Record start time when pulse is sent
+    pulse_start = time.time()
+
+    # Record end time when pulse is received
+    while GPIO.input(GPIO_ECHO) == 0:
+        pulse_start = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 1:
+        pulse_end = time.time()
+
+    # Calculate time difference
+    pulse_duration = pulse_end - pulse_start
+
+    return pulse_duration
+
+# Function to play sound with specified volume
+def play_sound_C(volume):
+    if volume > 70:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-C/3beep.mp3'])
+    elif volume > 45:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-C/2beep.mp3'])
+    else:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-C/1beep.mp3'])
+"""     
+def play_sound_L(volume):
+    if volume > 70:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-L/3beep-L.mp3'])
+    elif volume > 45:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-L/2beep-L.mp3'])
+    else:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-L/1beep-L.mp3'])
+        
+def play_sound_R(volume):
+    if volume > 70:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-R/3beep-R.mp3'])
+    elif volume > 45:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-R/2beep-R.mp3'])
+    else:
+        subprocess.run(['mpg321', '-g', str(volume), '/home/admin/Desktop/HColocation/beep-R/1beep-R.mp3'])
+"""
+
+# Main loop
+while True:
+    pulse_duration = measure_distance()
+    range_value = int(pulse_duration * 34300 / 2)
+
+    volume = int((400 - range_value) / 4.5)-10
+
+    print("Range: {} cm, Volume: {}%".format(range_value, volume))
+
+    play_sound_C(volume)
+
+    # Wait 0.1 seconds before taking another measurement
+    time.sleep(0.1)
